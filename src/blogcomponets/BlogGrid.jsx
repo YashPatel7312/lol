9d512aa4c3cc1cb4774fa563a44e9e4be950da2c
YashPatel7312 <your-github-email@example.com>
2025-10-30 09:58:26 +0530

    bloggrid.css deployment error solve

--- src/blogcomponets/BlogGrid.jsx
+++ src/blogcomponets/BlogGrid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../blogcomponets/BlogGrid.css   ";
+import "./BlogGrid.css";
 
 const BlogGrid = () => {
   const navigate = useNavigate();
